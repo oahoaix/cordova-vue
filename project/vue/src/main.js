f6dd9d4e616779c5f17a7a7a28f35feb02270cb2
yonghao.deng <281606634@qq.com>
2022-02-24 09:46:26 +0800

    init project

--- project/vue/src/main.js
+++ project/vue/src/main.js
@@ -7,9 +7,12 @@ import router from './router'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+document.addEventListener('deviceready', function(){
+  new Vue({
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
+  })
+}, false)
+  
